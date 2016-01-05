5a86ad0a8a9e2ff0165a54b5804ac39355c137fc
Eric Defore <eric.defore@gmail.com>
2016-01-05 12:48:29 -0500

    Made blogname and blogdescription available to Content Managers. Moved CTA Functionality to the Customizer. Closes #4. Closes #3

--- functions.php
+++ functions.php
@@ -97,4 +97,83 @@ function woo_phone_number() {
 
 // Woo phone number shortcode
 add_shortcode( 'woo_phone', 'woo_phone_number' );
+
+// Change capability needed to change options in the Customizer for things in WP Core
+add_action( 'customize_register', 'add_site_identity_for_non_admins' );
+function add_site_identity_for_non_admins( $wp_customize ) {
+    $wp_customize->get_setting( 'blogname' )->capability = 'edit_theme_options';
+    $wp_customize->get_setting( 'blogdescription' )->capability = 'edit_theme_options';
+}
+
+// Add Customizer Controls
+add_action( 'customize_register', 'genfinplan_customize_register' );
+function genfinplan_customize_register( $wp_customize ) {
+
+    $wp_customize->add_section( 'genfinplan_cta' , array(
+            'title'      => __( 'CTA Settings', 'genfinplan' ),
+            'priority'   => 30,
+            'active_callback' => 'is_front_page',
+        ) 
+    );
+    
+    $wp_customize->add_setting( 'genfinplan_cta_heading' , array(
+            'default'     => 'Helping a Select Group of Families Achieve their Life Goals',
+            'transport'   => 'refresh',
+        ) 
+    );
+    $wp_customize->add_control( new WP_Customize_Control( $wp_customize, 'genfinplan_cta_heading', array(
+        'label'        => __( 'CTA Heading', 'genfinplan' ),
+        'section'    => 'genfinplan_cta',
+        'settings'   => 'genfinplan_cta_heading',
+    ) ) );
+
+    // WP_Customize_Media_Control returns an Attachment ID instead of an Attachment URL, this way we can get whatever size we want
+    $wp_customize->add_setting( 'genfinplan_cta_image' , array(
+            'default'     => 160,
+            'transport'   => 'refresh',
+        ) 
+    );
+    $wp_customize->add_control( new WP_Customize_Media_Control( $wp_customize, 'genfinplan_cta_image', array(
+        'label'        => __( 'CTA Image', 'genfinplan' ),
+        'section'    => 'genfinplan_cta',
+        'mime_type' => 'image',
+    ) ) );
+    
+    $wp_customize->add_setting( 'genfinplan_cta_copy' , array(
+            'default'     => 'Servicing Southwest Michigan and clients nationwide, helping you make the right choices. We understand that the complexities of the Markets and Financial Vehicles can be daunting. Our combined experience of 19 years can help you plan and prepare for your life’s goals.',
+            'transport'   => 'refresh',
+        )
+    );
+    $wp_customize->add_control( new WP_Customize_Control( $wp_customize, 'genfinplan_cta_copy', array(
+        'type' => 'textarea',
+        'label'        => __( 'CTA Body Copy', 'genfinplan' ),
+        'section'    => 'genfinplan_cta',
+        'settings'   => 'genfinplan_cta_copy',
+    ) ) );
+    
+    $wp_customize->add_setting( 'genfinplan_cta_button_text' , array(
+            'default'     => 'Plan for my future',
+            'transport'   => 'refresh',
+        ) 
+    );
+    $wp_customize->add_control( new WP_Customize_Control( $wp_customize, 'genfinplagenfinplan_cta_button_textn_cta_heading', array(
+        'label'        => __( 'CTA Button Text', 'genfinplan' ),
+        'section'    => 'genfinplan_cta',
+        'settings'   => 'genfinplan_cta_button_text',
+    ) ) );
+    
+    $wp_customize->add_setting( 'genfinplan_cta_button_url' , array(
+            'default'     => '/wealth-management-services/financial-planning/',
+            'transport'   => 'refresh',
+        )
+    );
+    $wp_customize->add_control( new WP_Customize_Control( $wp_customize, 'genfinplan_cta_button_url', array(
+        'label'        => __( 'CTA Button Link', 'genfinplan' ),
+        'section'    => 'genfinplan_cta',
+        'settings'   => 'genfinplan_cta_button_url',
+    ) ) );
+    
+
+}
+
 ?>
--- includes/intro-message.php
+++ includes/intro-message.php
@@ -1,27 +1,17 @@
 <?php
-$settings = array(
-	'homepage_intro_message_heading'      => '',
-	'homepage_intro_message_content'      => '',
-	'homepage_intro_message_button_label' => '',
-	'homepage_intro_message_button_url'   => ''
-);
-
-$settings = woo_get_dynamic_values( $settings );
+// This is now handled via the Customizer. Easier (and more maintainable) to give the Client control over a single section of the Customizer than part of WooTheme's Theme Options Panel
 ?>
-<?php if ( '' != $settings['homepage_intro_message_heading'] ) { ?>
+<section id="intro-message">
+    <div class="col-full">
+        <div class="left-section">
+            <?php echo wp_get_attachment_image( get_theme_mod( 'genfinplan_cta_image', 160 ), 'intro-img' ); ?>
+            <h2><?php echo get_theme_mod( 'genfinplan_cta_heading', 'Helping a Select Group of Families Achieve their Life Goals' ); ?></h2>
 
-	<section id="intro-message">
-		<div class="col-full">
-			<div class="left-section">
-				<?php echo wp_get_attachment_image( '160', 'intro-img' ); ?>
-				<h2><?php echo $settings['homepage_intro_message_heading']; ?></h2>
-
-				<p><?php echo $settings['homepage_intro_message_content']; ?></p>
-			</div>
-			<div class="right-section">
-				<a class="button"
-				   href="<?php echo $settings['homepage_intro_message_button_url']; ?>"><?php echo $settings['homepage_intro_message_button_label']; ?></a>
-			</div>
-		</div>
-	</section><!--/#intro-message-->
-<?php } ?>
+            <p><?php echo get_theme_mod( 'genfinplan_cta_copy', 'Servicing Southwest Michigan and clients nationwide, helping you make the right choices. We understand that the complexities of the Markets and Financial Vehicles can be daunting. Our combined experience of 19 years can help you plan and prepare for your life’s goals.' ); ?></p>
+        </div>
+        <div class="right-section">
+            <a class="button"
+               href="<?php echo get_theme_mod( 'genfinplan_cta_button_url', '/wealth-management-services/financial-planning/' ); ?>"><?php echo get_theme_mod( 'genfinplan_cta_button_text', 'Plan for my future' ); ?></a>
+        </div>
+    </div>
+</section><!--/#intro-message-->
